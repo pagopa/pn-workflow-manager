ee368a767108b14dde46caca0aebc01feac70826
Andrea Bertucci <andrea.bertucci@pagopa.it>
2026-06-16 09:48:41 +0200

    Added dbOperations empty test

---
+++ functions/campaign-events-collector/src/test/dbOperations.test.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+const proxyquire = require("proxyquire");
+
+describe("dBOperations", () => {
+    it("first test", async () => {
+
+    });
+});
