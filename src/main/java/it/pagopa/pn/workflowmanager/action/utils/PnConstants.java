2a6410690f8061ae64368b6614d32889431f966a
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2026-07-28 12:20:55 +0200

    feat: fix

--- src/main/java/it/pagopa/pn/workflowmanager/action/utils/PnConstants.java
+++ src/main/java/it/pagopa/pn/workflowmanager/action/utils/PnConstants.java
@@ -1,13 +1,9 @@
 package it.pagopa.pn.workflowmanager.action.utils;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-
-@Component
-@Slf4j
-@AllArgsConstructor
 public final class PnConstants {
 
+    private PnConstants(){
+        //no usage
+    }
     public static final Integer FIRST_ATTEMPT = 0;
 }
