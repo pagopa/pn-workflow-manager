3deb36af20b785a03396773288904a827b76bff7
Stefano Rughetti <stefano.rughetti@emeal.nttdata.com>
2026-09-07 14:55:41 +0200

    [PN-21277] feat: add national registries client and configuration

--- src/test/java/it/pagopa/pn/workflowmanager/config/PnWorkflowManagerConfigsTest.java
+++ src/test/java/it/pagopa/pn/workflowmanager/config/PnWorkflowManagerConfigsTest.java
@@ -18,6 +18,7 @@ class PnWorkflowManagerConfigsTest {
                 .withProperty("pn.workflow-manager.timeline-client-base-url", "http://localhost:8093")
                 .withProperty("pn.workflow-manager.delivery-base-url", "http://localhost:8090")
                 .withProperty("pn.workflow-manager.action-manager-base-url", "http://localhost:8092")
+                .withProperty("pn.workflow-manager.national-registries-base-url", "http://localhost:8097")
                 .withProperty("pn.workflow-manager.topics.digital-queue", "pn-workflow-manager-digital-event-queue")
                 .withProperty("pn.workflow-manager.topics.analog-queue", "pn-workflow-manager-analog-event-queue")
                 .withProperty("pn.workflow-manager.topics.action-queue", "pn-workflow-manager-action-queue")
@@ -31,6 +32,7 @@ class PnWorkflowManagerConfigsTest {
 
         assertNotNull(pnNotificationCostServiceConfigs);
 
+        Assertions.assertEquals("http://localhost:8097", pnNotificationCostServiceConfigs.getNationalRegistriesBaseUrl());
 
         PnWorkflowManagerConfigs.Topics topics =
                 pnNotificationCostServiceConfigs.getTopics();
