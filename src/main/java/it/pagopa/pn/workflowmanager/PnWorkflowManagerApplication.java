0c979139db983a0983e965f0e287f1706fcfc772
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2026-09-18 18:13:41 +0200

    [PN-21486] feat: fix

--- src/main/java/it/pagopa/pn/workflowmanager/PnWorkflowManagerApplication.java
+++ src/main/java/it/pagopa/pn/workflowmanager/PnWorkflowManagerApplication.java
@@ -3,6 +3,8 @@ package it.pagopa.pn.workflowmanager;
 import it.pagopa.pn.commons.configs.listeners.TaskIdApplicationListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PnWorkflowManagerApplication {
@@ -16,4 +18,12 @@ public class PnWorkflowManagerApplication {
         return app;
     }
 
+    @RestController
+    public static class HomeController {
+
+        @GetMapping("")
+        public String home() {
+            return "Sono Vivo";
+        }
+    }
 }
